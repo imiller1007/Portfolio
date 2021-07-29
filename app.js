97cae348c1e9285cbb48ad6bc0ea0b07b29bf136
Ian Miller <imiller1007@yahoo.com>
2021-07-28 21:51:32 -0700

    updated portfolio for 2021!

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 //EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 
 //Routes
 app.use("/", require("./routes/index"));
+app.use("/test", require("./routes/test"));
 
 const PORT = process.env.PORT || 5000;
 
--- routes/test.js
+++ routes/test.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// index page
-router.get('/', (req, res) => res.render('index'));
+
 router.get('/test', (req, res) => res.render('test'));
 
 
